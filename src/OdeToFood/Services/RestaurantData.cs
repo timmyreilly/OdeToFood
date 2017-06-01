08c3af9e8ab5bffdf79c5832d0f180703e852b67
Tim Reilly <timreilly@live.com>
2017-06-01 16:38:58 -0700

    almost to partials

--- src/OdeToFood/Services/RestaurantData.cs
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -10,6 +10,7 @@ namespace OdeToFood.Services
         IEnumerable<Restaurant> GetAll();
         Restaurant Get(int id);
         Restaurant Add(Restaurant newRestaurant);
+        void Commit();
     }
 
     public class SqlRestaurantData : IRestaurantData
@@ -26,6 +27,11 @@ namespace OdeToFood.Services
             _context.Add(newRestaurant);
             _context.SaveChanges(); 
             return newRestaurant; 
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges(); 
         }
 
         public Restaurant Get(int id)
@@ -67,6 +73,11 @@ namespace OdeToFood.Services
             _restaurants.Add(newRestaurant);
 
             return newRestaurant; 
+        }
+
+        public void Commit()
+        {
+            // .... no op 
         }
 
         static List<Restaurant> _restaurants; 
